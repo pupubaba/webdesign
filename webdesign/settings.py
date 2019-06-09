543dd8af0c1308abe338f84491b7a9b9d0b48c06
pupubaba <kaki1203@naver.com>
2019-06-10 05:19:57 +0900

    third commit

--- webdesign/settings.py
+++ webdesign/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('f0tqzkgb$aa!j=qbw3&8*_u6(usi#23^^0$sz6axsu6)c9$nuv')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'f0tqzkgb$aa!j=qbw3&8*_u6(usi#23^^0$sz6axsu6)c9$nuv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANOG_DEBUG',True))
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
